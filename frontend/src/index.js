3258f383832c9fe501bf941c53decd618518ab78
madanodyblum <gorayblum@gmail.com>
2019-09-28 23:51:40 +0100

    project update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import { store, history} from './store';
 import { Route, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
 
-import App from './components/App';
+import Apps from './components/App';
 
 ReactDOM.render((
   <Provider store={store}>
